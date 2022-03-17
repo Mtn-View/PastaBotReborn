8211dfbc6379796feaf5303f838cc3bc489fd9bb
Mtn-View <cv.kaup@gmail.com>
2022-03-16 21:56:18 -0500

    Make the deploy commands suck less

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,10 +1,15 @@
 const { SlashCommandBuilder, SlashCommandUserOption } = require('@discordjs/builders')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
-const { clientId, guildId } = require('./config.json')
-const { token } = require('./auth.json')
+const { clientId, guildId, prodClientId } = require('./config.json')
+const { token, prodToken } = require('./auth.json')
 
 const ownerIds = [ 158763732254588928, 221753632566018061 ] // Aquahawk, CharlesKaup
+
+const isDev = process.argv.includes('dev')
+console.log("Dev?", isDev)
+const rest = new REST({ version: '9' }).setToken(isDev ? token : prodToken)
+const currentClientId = isDev ? clientId : prodClientId
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
@@ -135,18 +140,63 @@ const commands = [
 ]
 	.map(command => command.toJSON())
 
-const rest = new REST({ version: '9' }).setToken(token)
+async function deleteAllGuildCommands() {
+	const guildCommands = await rest.get(Routes.applicationGuildCommands(currentClientId, guildId))
+	const promises = []
+	for (const command of guildCommands) {
+		const deleteUrl = `${Routes.applicationGuildCommands(currentClientId, guildId)}/${command.id}`
+		promises.push(rest.delete(deleteUrl))
+	}
+	return Promise.all(promises)
+}
 
-// rest.get(Routes.applicationGuildCommands(clientId, guildId))
-// 	.then(data => {
-// 		const promises = []
-// 		for (const command of data) {
-// 			const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`
-// 			promises.push(rest.delete(deleteUrl))
-// 		}
-// 		return Promise.all(promises)
-// 	})
+async function deleteAllGlobalCommands() {
+	const globalCommands = await rest.get(Routes.applicationCommands(currentClientId))
+	return await Promise.all(globalCommands.map(command => {
+		const deleteURL = `${Routes.applicationCommands(currentClientId)}/${command.id}`
+		return rest.delete(deleteURL)
+	}))
+}
 
-// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-// 	.then(() => console.log('Successfully registered application commands.'))
-// 	.catch(console.error)
+async function start() {
+	if (process.argv.includes('deleteGlobal')) {
+		try {
+			console.log('Deleting global commands')
+			const res = await deleteAllGlobalCommands()
+			console.log("Deleted global commands")
+		} catch (err) {
+			console.error(err)
+		}
+	}
+	if (process.argv.includes('deleteGuild')) {
+		try {
+			console.log('Deleting guild commands')
+			const res = await deleteAllGuildCommands()
+			console.log("Deleted guild commands")
+		} catch (err) {
+			console.error(err)
+		}
+	}
+	if (process.argv.includes('registerGlobal')) {
+		try {
+			console.log('Registering global commands')
+			const res = await rest.put(Routes.applicationCommands(currentClientId), { body: commands })
+			console.log('Registered application commands globally.')
+		} catch (err) {
+			console.error(err)
+		}
+	}
+	if (process.argv.includes('registerGuild')) {
+		try {
+			console.log('Registering guild commands')
+			const res = await rest.put(Routes.applicationGuildCommands(currentClientId, guildId), { body: commands })
+			console.log('Registered application commands for guild.')
+		} catch (err) {
+			console.error(err)
+		}
+	}
+}
+start().then(()=> {
+	console.log('Completed')
+})
+
